e6021d301f9dbce6d8468c3b78e1a4006c5e2c17
jfstassen <jf.stassen@gmail.com>
2020-04-05 17:22:44 +0200

    add 06 - CLI

---
+++ 06-vue-cli/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <appHeader></appHeader>
+        <hr>
+        <div class="row">
+            <Servers></Servers> <!-- can also use servers -->
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Shared/Header.vue';
+    import Footer from './components/Shared/Footer.vue';
+    import Servers from './components/Server/Servers.vue';
+    import ServerDetails from './components/Server/ServerDetails.vue';
+
+    export default {
+        components: {
+            appHeader: Header, // without -
+            Servers, //ES6 shorthand
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        }
+    }
+</script>
+
+<style>
+
+</style>

425c187db1b3a4b334ebdf397ba85b45739ab86b
jfstassen <jf.stassen@gmail.com>
2020-04-06 20:51:50 +0200

    finish exercice 10

--- 10-exercice-slot-and-dynamic-comp/src/App.vue
+++ 10-exercice-slot-and-dynamic-comp/src/App.vue
@@ -1,32 +1,36 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
-                <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <br />
+        <button @click="activeComponent = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
+        <button @click="activeComponent = 'appGreen'" class="btn btn-success">Load Green Template</button>
+        <button @click="activeComponent = 'appRed'" class="btn btn-danger">Load Red Template</button>
+        <hr />
+        <component :is="activeComponent">
+          <p slot="blue">test blue slot</p>
+        </component>
+
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import Blue from './components/Blue.vue';
-    import Green from './components/Green.vue';
-    import Red from './components/Red.vue';
+import Blue from "./components/Blue.vue";
+import Green from "./components/Green.vue";
+import Red from "./components/Red.vue";
 
-    export default {
-        components: {
-            appBlue: Blue,
-            appGreen: Green,
-            appRed: Red
-        }
-    }
+export default {
+  components: {
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red
+  },
+  data: () => ({
+    activeComponent: ""
+  })
+};
 </script>
 
 <style>
--- 10-exercice-slot-and-dynamic-comp/src/components/Blue.vue
+++ 10-exercice-slot-and-dynamic-comp/src/components/Blue.vue
@@ -1,5 +1,7 @@
 <template>
-    <div></div>
+    <div>
+        <slot name="blue"></slot>
+    </div>
 </template>
 
 <script>

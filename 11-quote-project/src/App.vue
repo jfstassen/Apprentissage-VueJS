93f13d941b7ea87258c5e6b1c531c44810f8ab54
jfstassen <jf.stassen@gmail.com>
2020-04-07 19:34:18 +0200

    add 11 initial upto deleteQuote

---
+++ 11-quote-project/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <!-- Listen to QuoteAdded and execute NewQuotefn -->
+    <NewQuote @quoteAdded="newQuoteFn"></NewQuote>
+    <QuoteGrid :quotes="quotes" @quoteDeleted="deleteQuoteFn"></QuoteGrid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Click on a quote to delete</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid";
+import NewQuote from "./components/NewQuote";
+
+export default {
+  data: () => ({
+    quotes: ["Just a quote to see something"],
+    maxQuotes: 10
+  }),
+  components: {
+    QuoteGrid,
+    NewQuote
+  },
+  methods: {
+    newQuoteFn(quote) {
+      this.quotes = [quote, ...this.quotes];
+      // this.quotes.push(quote);
+    },
+    deleteQuoteFn(index) {
+      // this.quotes.splice(index, 1);
+      this.quotes = this.quotes.filter((item, i) => i !== index)
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ 11-quote-project/src/components/QuoteGrid.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+    <!-- .native click on component -->
+    <quote @click.native="deleteQuote(index)" v-for="(quote, index) in quotes" :key="index">
+      {{quote}}
+      <small>{{index}}</small>
+    </quote>
+  </div>
+</template>
+
+<script>
+import Quote from "./Quote";
+export default {
+  props: {
+    quotes: { type: Array }
+  },
+  components: {
+    Quote
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    }
+  }
+};
+</script>
+
+<style>
+</style>

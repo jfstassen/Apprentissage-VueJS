04c3e4a90c0bba4cc37b121998456efb66a68b65
jfstassen <jf.stassen@gmail.com>
2020-04-09 15:56:55 +0200

    add global filters and local filters

--- 14-filters-and-mixins/src/App.vue
+++ 14-filters-and-mixins/src/App.vue
@@ -3,18 +3,23 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
+        {{text | toUppercase | to-lowercase}}
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      text: "hello there"
+    };
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    }
   }
 };
 </script>
--- 14-filters-and-mixins/src/main.js
+++ 14-filters-and-mixins/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.filter("to-lowercase", (value) => {
+  return value.toLowerCase();
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");

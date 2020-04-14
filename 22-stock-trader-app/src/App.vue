4949f80b513263d58f839dfae16ccc8f3c60e64b
jfstassen <jf.stassen@gmail.com>
2020-04-14 17:06:54 +0200

    starting

---
+++ 22-stock-trader-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <Header></Header>
+    <div class="row">
+      <div class="col-md-12">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+body {
+  padding: 30px;
+}
+</style>
---
+++ 22-stock-trader-app/src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <router-link to="/" class="navbar-brand" href="#">Stock Trader</router-link>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <router-link tag="li" to="/portfolio" activeClass="active">
+            <a>Portfolio</a>
+          </router-link>
+          <router-link tag="li" to="/stocks" activeClass="active">
+            <a>Stocks</a>
+          </router-link>
+        </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <a href="#">End Day</a>
+          </li>
+          <li class="dropdown">
+            <a
+              href="#"
+              class="dropdown-toggle"
+              data-toggle="dropdown"
+              role="button"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Save & Load
+              <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a href="#">Save</a>
+              </li>
+              <li>
+                <a href="#">Load</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

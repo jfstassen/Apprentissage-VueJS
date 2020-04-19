3db281ba4b94a6ad14fb050e519c157d36394ddd
jfstassen <jf.stassen@gmail.com>
2020-04-19 18:56:24 +0200

    add v-intersect

--- 24-vuetify-dashboard-suite/src/views/Dashboard.vue
+++ 24-vuetify-dashboard-suite/src/views/Dashboard.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
     <h1>Dashboard</h1>
+
     <v-row>
       <v-col v-for="sale in sales" :key="`${sale.title}`" cols="12" md="4">
         <SalesGraph :sale="sale" />
@@ -8,21 +9,36 @@
     </v-row>
 
     <v-row>
-      <v-col v-for="statistic in statistics" :key="`${statistic.title}`" cols='12' md="6" lg="3">
+      <v-col v-for="statistic in statistics" :key="`${statistic.title}`" cols="12" md="6" lg="3">
         <StatisticCard :statistic="statistic" />
       </v-col>
     </v-row>
+
     <v-row>
       <v-col cols="12" md="8">
         <EmployeesTable :employees="employees" @select-employee="setEmployee" />
       </v-col>
-
       <v-col cols="12" md="4">
         <EventTimeline :timeline="timeline" />
       </v-col>
     </v-row>
 
-    <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lg">
+    <v-row id="below-the-fold" v-intersect="showMoreContent">
+      <v-col cols="12" md="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <v-row v-if="loadNewContent" id="more-content">
+      <v-col>
+        <v-skeleton-loader ref="skeleton" type="table" class="mx-auto"></v-skeleton-loader>
+      </v-col>
+    </v-row>
+
+    <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lgAndUp">
       You have selected {{ selectedEmployee.name }},
       {{ selectedEmployee.title }}
       <v-btn color="pink" text @click="snackbar = false">Close</v-btn>
@@ -35,12 +51,10 @@ import EmployeesTable from "../components/EmployeesTable";
 import EventTimeline from "../components/EventTimeline";
 import SalesGraph from "../components/SalesGraph";
 import StatisticCard from "../components/StatisticCard";
-
 import employeesData from "../data/employees.json";
 import timelineData from "../data/timeline.json";
 import salesData from "../data/sales.json";
 import statisticsData from "../data/statistics.json";
-
 export default {
   name: "DashboardPage",
   components: {
@@ -52,6 +66,7 @@ export default {
   data() {
     return {
       employees: employeesData,
+      loadNewContent: false,
       sales: salesData,
       selectedEmployee: {
         name: "",
@@ -67,6 +82,9 @@ export default {
       this.snackbar = true;
       this.selectedEmployee.name = event.name;
       this.selectedEmployee.title = event.title;
+    },
+    showMoreContent(entries){
+      this.loadNewContent = entries[0].isIntersecting
     }
   }
 };

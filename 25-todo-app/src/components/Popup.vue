943e8badb120af71dc3bff5e5a0259fe12a9e446
jfstassen <jf.stassen@gmail.com>
2020-05-08 15:12:47 +0200

    add dialog

--- 25-todo-app/src/components/Popup.vue
+++ 25-todo-app/src/components/Popup.vue
@@ -8,13 +8,33 @@
         <v-card-title>
           <span class="headline">User Profile</span>
         </v-card-title>
+        <v-card-text>
+          <v-form class="px-3">
+            <v-text-field v-model="title" label="Title" prepend-icon="mdi-folder"></v-text-field>
+            <v-textarea v-model="content" label="Information" prepend-icon="mdi-pencil"></v-textarea>
+            <v-btn text class="success mx-0 mt-3" @click="submit">Add Project</v-btn>
+          </v-form>
+        </v-card-text>
       </v-card>
     </v-dialog>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return {
+      title:"",
+      content:""
+    }
+  },
+  methods:{
+    submit(){
+      console.log(this.title, this.content)
+    }
+  }
+    
+  }
 </script>
 
 <style>

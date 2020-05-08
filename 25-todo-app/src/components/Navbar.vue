04f984c3fa6f1d2fca29547f52cd74607a9c8897
jfstassen <jf.stassen@gmail.com>
2020-05-08 13:52:28 +0200

    add popup comp

--- 25-todo-app/src/components/Navbar.vue
+++ 25-todo-app/src/components/Navbar.vue
@@ -2,11 +2,15 @@
   <nav>
     <v-navigation-drawer v-model="drawer" app class="primary">
       <v-row justify="center" class="my-3">
-       <v-col cols="auto" align="center">
+        <v-col cols="auto" align="center">
           <v-avatar size="100">
-            <img src="https://api.adorable.io/avatars/100/g@adorable.png"/>
+            <img src="https://api.adorable.io/avatars/100/b@adorable.png" />
           </v-avatar>
-          <p class="white--text subtitle my-3" >Mister Thomas</p>
+          <p class="white--text subtitle my-3">Ryu</p>
+        </v-col>
+        <!-- create project btn -->
+        <v-col align="center" class="mt-4 mb-3">
+          <Popup />
         </v-col>
       </v-row>
       <v-list>
@@ -22,12 +26,28 @@
     </v-navigation-drawer>
 
     <v-app-bar flat app>
+      <!-- burger & App name -->
       <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>App</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <!-- dropdown menu -->
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn color="grey" dark text v-on="on">
+            <v-icon left>mdi-chevron-down</v-icon>
+            <span>Menu</span>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(link, index) in links" :key="index" :to="link.route">
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <!-- sign off btn -->
       <v-btn text color="grey">
         <span>Sign Out</span>
         <v-icon right>mdi-exit-to-app</v-icon>
@@ -37,7 +57,11 @@
 </template>
 
 <script>
+import Popup from "@/components/Popup";
 export default {
+  components: {
+    Popup
+  },
   data() {
     return {
       drawer: true,
---
+++ 25-todo-app/src/components/Popup.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-dialog max-width="600px">
+      <template v-slot:activator="{on}">
+        <v-btn text class="success" @click.stop="dialog = true" v-on="on">Add new Project</v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

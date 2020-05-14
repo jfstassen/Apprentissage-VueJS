f6ad13616194c198f405a851ffcc5a9c6c092227
jfstassen <jf.stassen@gmail.com>
2020-05-14 12:43:04 +0200

    add 26

---
+++ 26-real-world-vue/.prettierrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  singleQuote: true, //convert double quote to single
+  semi: false
+}
---
+++ 26-real-world-vue/src/views/EventShow.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Events Listing #{{id}}</h1>
+</template>
+<script>
+export default {
+  props: ['id']
+}
+</script>

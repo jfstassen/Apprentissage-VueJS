9506961ea858e8957d12e620830cef0520130139
jfstassen <jf.stassen@gmail.com>
2020-05-15 21:43:08 +0200

    add vueX: mapState

--- 26-real-world-vue/src/store.js
+++ 26-real-world-vue/src/store.js
@@ -4,7 +4,36 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ]
+  },
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: (state, getters) => {
+      //use a getters inside a getters (just an example)
+      return state.todos.length - getters.doneTodos.length
+    }
+  }
 })
--- 26-real-world-vue/src/views/EventCreate.vue
+++ 26-real-world-vue/src/views/EventCreate.vue
@@ -1,3 +1,44 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <!-- <h1>Create Event, {{userName}}</h1> -->
+    <h1>Create Event, {{user.name}}</h1>
+    <p>This event was created by {{user.id}}</p>
+    <p>There are {{catLength}} categories</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{cat}}</li>
+    </ul>
+  </div>
 </template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed:
+    // userName(){
+    //   return this.$store.state.user.name
+    // }
+
+    // mapState({
+    //   userName: state => state.user.name,
+    //   userID: state => state.user.id,
+    //   categories: state => state.categories
+    // })
+
+    // mapState({
+    //   user: 'user',
+    //   categories: 'categories'
+    // })
+
+    // mapState(['user', 'categories'])
+
+    {
+      // pour eviter de faire du computed sur du $store.state on fait ca sur un getters et on l'appelle ensuite
+      catLength() {
+        return this.$store.getters.catLength
+      },
+      ...mapState({
+        user: 'user',
+        categories: 'categories'
+      })
+    }
+}
+</script>
